445452cf1964fdab23154536d220db5dc7a165a5
MilapDave111 <milapdave6355@gmail.com>
2026-01-17 20:14:27 +0530

    Latency Fixed

--- backend/server.js
+++ backend/server.js
@@ -38,3 +38,7 @@ app.use('/api/subjects', subjectRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
